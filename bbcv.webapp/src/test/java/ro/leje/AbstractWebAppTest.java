8d7bc307ee4d11361191754d8ee69695f8a293d2
danutchindris <danutchindris@gmail.com>
2015-08-14 00:27:27 +0300

    small changes on the webapp test classes

--- bbcv.webapp/src/test/java/ro/leje/AbstractWebAppTest.java
+++ bbcv.webapp/src/test/java/ro/leje/AbstractWebAppTest.java
@@ -1,12 +1,12 @@
 package ro.leje;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * @author Danut Chindris
@@ -15,7 +15,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class ApplicationTest {
+@ActiveProfiles(profiles = "localtest")
+public class AbstractWebAppTest {
 
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 }
