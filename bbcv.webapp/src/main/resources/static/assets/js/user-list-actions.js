e273b301a5b28d51f229a69b4e5a47b908a91a8c
danutchindris <danutchindris@gmail.com>
2015-09-07 17:58:20 +0300

    some more work on the admin dashboard and the user list

--- bbcv.webapp/src/main/resources/static/assets/js/user-list-actions.js
+++ bbcv.webapp/src/main/resources/static/assets/js/user-list-actions.js
@@ -5,28 +5,6 @@ var $table = $('#fresh-table'),
     full_screen = false;
 
 $().ready(function() {
-    $table.bootstrapTable({
-        showToggle: true,
-        showColumns: true,
-        striped: true,
-        pageSize: 8,
-        pageList: [8,10,25,50,100],
-
-        formatShowingRows: function(pageFrom, pageTo, totalRows){
-            //do nothing here, we don't want to show the text "showing x of y from..."
-        },
-        formatRecordsPerPage: function(pageNumber){
-            return pageNumber + " rows visible";
-        },
-        icons: {
-            refresh: 'fa fa-refresh',
-            toggle: 'fa fa-th-list',
-            columns: 'fa fa-columns',
-            detailOpen: 'fa fa-plus-circle',
-            detailClose: 'fa fa-minus-circle'
-        }
-    });
-
     $(window).resize(function () {
         $table.bootstrapTable('resetView');
     });
