1f3e6f2180a2a29aba9d5258ff10bdc712b2418a
danutchindris <danutchindris@gmail.com>
2016-09-14 21:03:03 +0200

    #3: draft implementation of the article details page

--- bbcv.webapp/src/main/java/ro/leje/controller/ArticlesController.java
+++ bbcv.webapp/src/main/java/ro/leje/controller/ArticlesController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ro.leje.delegate.LanguageDelegate;
+import ro.leje.model.vo.Article;
 import ro.leje.service.ArticleService;
 import ro.leje.util.MappingConstants;
 import ro.leje.util.ViewConstants;
 
 import javax.annotation.Resource;
+import java.util.Locale;
+import java.util.Optional;
 
 /**
  * @author Danut Chindris
@@ -20,6 +23,9 @@ import javax.annotation.Resource;
 @RequestMapping(value = MappingConstants.ARTICLES)
 public class ArticlesController {
 
+    private static final String ARTICLE_MODEL_ATTRIBUTE = "article";
+    private static final String ARTICLE_ID_PATH_VARIABLE = "articleId";
+
     @Resource
     private ArticleService articleService;
 
@@ -27,9 +33,13 @@ public class ArticlesController {
     private LanguageDelegate languageDelegate;
 
     @RequestMapping(value = MappingConstants.ARTICLE_DETAILS, method = RequestMethod.GET)
-    public String getArticleDetails(Model model, @PathVariable("articleId") long articleId) {
+    public String getArticleDetails(Model model, Locale locale, @PathVariable(ARTICLE_ID_PATH_VARIABLE) long articleId) {
         languageDelegate.addAvailableLanguages(model);
         languageDelegate.addNotAvailableLanguages(model);
+        Optional<Article> article = articleService.find(articleId, locale.getLanguage());
+        if (article.isPresent()) {
+            model.addAttribute(ARTICLE_MODEL_ATTRIBUTE, article.get());
+        }
         return ViewConstants.ARTICLE_DETAILS;
     }
 }
