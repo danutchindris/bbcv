bc611da9bdbcc373480d8c9511374143c3ad58d4
danutchindris <danutchindris@gmail.com>
2017-07-29 07:25:32 +0300

    #36 - add 'destinations' to the non-secured paths list

--- bbcv.webapp/src/main/java/ro/leje/config/WebSecurityConfig.java
+++ bbcv.webapp/src/main/java/ro/leje/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         String[] staticContent = {"/assets/**", "/images/**"};
         String[] accessibleToAllUrlPatterns = {"/error", "/", "/home", "/we-believe", "/you-are-invited",
-                "/links", "/contact-us", "/articles/**", "/change-language/**"};
+                "/links", "/contact-us", "/articles/**", "/change-language/**", "/destinations/**"};
         // @formatter:off
         http
                 .authorizeRequests()
